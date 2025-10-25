6adf26ba9fddd3b7d2762e5d1b21912c8305693d
Michael Rodriguez <michaelrove901@gmail.com>
2025-10-25 16:23:58 -0500

    Configuracion deploy android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'temp-todo-app',
+  appId: 'com.michaelrodriguez.todolistapp',
+  appName: 'Todo App',
   webDir: 'www'
 };
 
