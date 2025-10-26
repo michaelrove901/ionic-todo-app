6dd4672e3b787ea881b81c5f61b464dc7b00dccc
Michael Rodriguez <michaelrove901@gmail.com>
2025-10-25 21:16:03 -0500

    Actualizar environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDVWifZ98L5b7iKTrLAIjjvsWrIH3yCpYI",
+    authDomain: "todo-app-13d9a.firebaseapp.com",
+    projectId: "todo-app-13d9a",
+    storageBucket: "todo-app-13d9a.firebasestorage.app",
+    messagingSenderId: "142941458914",
+    appId: "1:142941458914:web:53f471eb38d49799d829bb",
+    measurementId: "G-F8HK069C1B"
+  }
 };
